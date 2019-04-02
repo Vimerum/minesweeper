4aaadedba10393b887f11d12095f85d9496ec631
Vinícius Veronese Gonçalves <vi.veronese@gmail.com>
2019-04-02 16:41:43 -0300

    Initial commit

---
+++ Minesweeper 2000/Assets/_Scripts/Cell.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isBomb;
+    [HideInInspector]
+    public bool isHidden;
+    [HideInInspector]
+    public bool isFlagged;
+    [HideInInspector]
+    public int level;
+
+    private SpriteRenderer spriteRenderer;
+    private TextMeshPro levelText;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        levelText = GetComponentInChildren<TextMeshPro>();
+        Initialize();
+    }
+
+    private void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0))
+            Open();
+        if (Input.GetMouseButtonDown(1))
+            Flag();
+    }
+
+    private void Initialize () {
+        // TODO: To be removed, since this values will be set by the GameManager
+        isBomb = false;
+        isHidden = true;
+        isFlagged = false;
+        level = 0;
+    }
+
+    private void Open () {
+        if (isFlagged) return;
+        if (!isHidden) return;
+
+        Debug.Log("[Cell.Open()]: Cell.Open() called");
+        isHidden = false;
+
+        if (isBomb) {
+            Debug.Log("[Cell.Open()]: It's a bomb");
+            spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Bomb Cell");
+            GameManager.instance.GameOver();
+        }
+        else {
+            Debug.Log("[Cell.Open()]: Empty cell");
+            spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Empty Cell");
+            if (level > 0) {
+                Debug.Log("[Cell.Open()]: Has level " + level);
+                levelText.text = level.ToString();
+            }
+        }
+    }
+
+    private void Flag () {
+        if (!isHidden) return;
+
+        isFlagged = !isFlagged;
+        Debug.Log("[Cell.Flag()]: isFlagged=" + isFlagged.ToString());
+
+        if (isFlagged)
+            spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Marked Cell");
+        else
+            spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Blank Cell");
+    }
+}
---
+++ Minesweeper 2000/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("Grid Configuration")]
+    public Vector2 gridSize;
+    public int numberBombs;
+    [Header("Grid Configuration")]
+    public GameObject cellPrefab;
+
+    private List<GameObject> cells;
+
+    public void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void Start() {
+        Initialize();
+    }
+
+    private void Initialize () {
+        cells = new List<GameObject>();
+        for (int y = 0; y > -gridSize.y; y--) {
+            for (int x = 0; x < gridSize.x; x++) {
+                GameObject go = Instantiate(cellPrefab, new Vector3(transform.position.x + x, transform.position.y + y), Quaternion.identity, transform);
+                cells.Add(go);
+            }
+        }
+    }
+
+    public void GameOver () {
+        Debug.Log("GAME OVER!!");
+    }
+}
