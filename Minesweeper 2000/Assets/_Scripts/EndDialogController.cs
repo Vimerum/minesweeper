c7e601f38fd1a2f1280598e4da32655c3e1f67d4
Vinícius Veronese Gonçalves <vi.veronese@gmail.com>
2019-04-20 01:57:05 -0300

    Added controller for dialog at the end game

---
+++ Minesweeper 2000/Assets/_Scripts/EndDialogController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndDialogController : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public TextMeshProUGUI endGameText;
+    public TextMeshProUGUI timerText;
+
+    private ButtonClick restartCallback;
+    private ButtonClick backToMenuCallback;
+
+    public void Initialize (string endGameMessage, float time, ButtonClick restartCallback, ButtonClick backToMenuCallback) {
+        endGameText.text = endGameMessage;
+        timerText.text = string.Format("Tempo: {0:00}:{1:00}", (Mathf.FloorToInt(time) / 60), (Mathf.Floor(time) % 60));
+        this.restartCallback = restartCallback;
+        this.backToMenuCallback = backToMenuCallback;
+    }
+
+    public void Restart () {
+        restartCallback();
+        Destroy(gameObject);
+    }
+
+    public void BackToMenu () {
+        backToMenuCallback();
+        Destroy(gameObject);
+    }
+}
